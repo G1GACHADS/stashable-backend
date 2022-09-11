a5b4b4335d8c6ae31a04b8cc77c53e547186b504
HotPotatoC <juandotulung@gmail.com>
2022-09-12 01:55:04 +0700

    feat(warehouse_search): fixed query

--- backend/warehouse_search.go
+++ backend/warehouse_search.go
@@ -18,7 +18,14 @@ type WarehouseSearchOutput struct {
 }
 
 func (b *backend) WarehouseSearch(ctx context.Context, searchQuery string, limit int, priceAscending bool) (WarehouseSearchOutput, error) {
-	sql := b.makeSearchWarehouseQuery(priceAscending)
+	sql := `SELECT * FROM warehouses_list
+	WHERE
+		name ILIKE '%' || $1 || '%' OR
+		province ILIKE '%' || $1 || '%' OR
+		city ILIKE '%' || $1 || '%' OR
+		street_name ILIKE '%' || $1 || '%'
+	LIMIT $2
+	`
 
 	rows, err := b.clients.DB.Query(ctx, sql, searchQuery, limit)
 	if err != nil {
@@ -31,6 +38,7 @@ func (b *backend) WarehouseSearch(ctx context.Context, searchQuery string, limit
 	for rows.Next() {
 		var row WarehouseSearchItem
 		err := rows.Scan(
+			nil,
 			&row.Attributes.ID,
 			&row.Attributes.AddressID,
 			&row.Attributes.Name,
@@ -40,6 +48,7 @@ func (b *backend) WarehouseSearch(ctx context.Context, searchQuery string, limit
 			&row.Attributes.Email,
 			&row.Attributes.PhoneNumber,
 			&row.Attributes.CreatedAt,
+			&row.Attributes.RoomsCount,
 			&row.Relationships.Address.ID,
 			&row.Relationships.Address.Province,
 			&row.Relationships.Address.City,
@@ -57,36 +66,4 @@ func (b *backend) WarehouseSearch(ctx context.Context, searchQuery string, limit
 		TotalItems: len(warehouses),
 		Items:      warehouses,
 	}, nil
-}
-
-func (b *backend) makeSearchWarehouseQuery(priceAscending bool) string {
-	mainQuery := `SELECT
-		w.*,
-		a.*,
-		array_agg(c.name)
-	FROM warehouses w
-		LEFT JOIN addresses AS a ON w.address_id = a.id
-		LEFT JOIN warehouse_categories AS wc ON w.id = wc.warehouse_id
-		LEFT JOIN categories AS c ON wc.category_id = c.id
-	WHERE
-		w.name ILIKE '%' || $1 || '%' OR
-		w.description ILIKE '%' || $1 || '%' OR
-		w.email ILIKE '%' || $1 || '%' OR
-		a.province ILIKE '%' || $1 || '%' OR
-		a.city ILIKE '%' || $1 || '%' OR
-		a.street_name ILIKE '%' || $1 || '%' OR
-		c.name ILIKE '%' || $1 || '%'
-	GROUP BY w.id,
-		a.id
-	`
-
-	if priceAscending {
-		mainQuery += "ORDER BY w.base_price ASC"
-	} else {
-		mainQuery += "ORDER BY w.base_price DESC"
-	}
-
-	mainQuery += " LIMIT $2"
-
-	return mainQuery
 }
